f845b648f96694b5b333a3f300584fbdee08ac3d
SergeyGoncharov <mr.serhioru@mail.ru>
2023-12-29 19:33:33 +0300

    Add project files.

---
+++ Angulyr_ASP/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Angulyr_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Angulyr_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Product[] products = Product.GetProducts();
+            return View(products);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Angulyr_ASP/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Angulyr_ASP.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;  
+        public decimal Price { get; set; }    
+        public  static Product[] GetProducts()
+        {
+            Product board = new Product() { 
+                Name = "Board", Price = 300
+            }; 
+            Product ball = new Product() { 
+                Name = "Ball", Price = 50
+            };
+            return new Product[] { board, ball };
+        }
+    }
+}
