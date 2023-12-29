25612e4a708f660ad25288c8430ac180ea968498
SergeyGoncharov <mr.serhioru@mail.ru>
2023-12-29 19:57:14 +0300

    Новые товары

--- SportStore/Models/SeedData.cs
+++ SportStore/Models/SeedData.cs
@@ -16,43 +16,55 @@ namespace SportStore.Models
 					
 					new Product
 					{
-						Name= "Мяч",Description = "Круглый ",
-						Category = "мячи",Price =100
+						Name= "Меч",Description = "Полуторный Игрушечный",
+						Category = "мечи",Price =600
 					},
 					new Product
 					{
 						Name = "Лодка",
-						Description = "Резиновая удобная",
+						Description = "Пластмассовая не удобная",
 						Category = "Рыболовство",
-						Price = 1000
+						Price = 1500
 					},
 					new Product
 					{
 						Name = "Спасательный жилет",
-						Description = "Оранжевый",
+						Description = "Бело-рыжий",
 						Category = "Рыболовство",
-						Price = 200
+						Price = 300
 					},
 					new Product
 					{
-						Name = "Гантели",
-						Description = "Тяжелые ",
+						Name = "Гантеля",
+						Description = "2кг ",
 						Category = "Спорт",
 						Price = 100
-					},
-					new Product
+					}, new Product
+                    {
+                        Name = "Гантеля",
+                        Description = "4кг ",
+                        Category = "Спорт",
+                        Price = 200
+                    }, new Product
+                    {
+                        Name = "Гантеля",
+                        Description = "6кг ",
+                        Category = "Спорт",
+                        Price = 600
+                    },
+                    new Product
 					{
 						Name = "Эспандер",
-						Description = "Полезный",
+						Description = "Пружинный",
 						Category = "Спорт",
-						Price = 120
+						Price = 220
 					},
 					new Product
 					{
 						Name = "Турник",
-						Description = "Домашний",
+						Description = "Настенный с крепежем",
 						Category = "Спорт",
-						Price = 400
+						Price = 800
 					},
 					new Product
 					{
@@ -60,9 +72,16 @@ namespace SportStore.Models
 						Description = "Беговые",
 						Category = "Спорт",
 						Price = 800
-					}
+					},
+                    new Product
+                    {
+                        Name = "Шапка резина",
+                        Description = "Бассейн",
+                        Category = "Спорт",
+                        Price = 300
+                    }
 
-					);
+                    );
 				context.SaveChanges();
 			}
 
