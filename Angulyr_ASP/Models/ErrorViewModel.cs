abdbefe29325c8fb0860f1d650d6835cf55d24bc
SergeyGoncharov <mr.serhioru@mail.ru>
2023-12-29 19:59:47 +0300

    изменения

--- Angulyr_ASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Angulyr_ASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
